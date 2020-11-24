1ed2dbeb54aa5cdce7cb5c7d2d7a936a4b010879
anujmehta-119 <55618672+anujmehta-119@users.noreply.github.com>
2020-11-25 02:18:13 +0530

    fin ii

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>SMART CITY</h1>
+        <h1>Smart City
+        </h1>
       </header>
     </div>
   );
